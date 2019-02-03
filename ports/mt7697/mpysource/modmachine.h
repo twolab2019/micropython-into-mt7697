7234d9b3de2a056f90736f51d2454bbffc758f29
onionys <onionys@gmail.com>
2019-02-03 14:03:28 +0800

    mt7697/machine_wdt.c.h: add watch dog

--- ports/mt7697/mpysource/modmachine.h
+++ ports/mt7697/mpysource/modmachine.h
@@ -12,7 +12,7 @@ extern const mp_obj_type_t machine_adc_type;
 extern const mp_obj_type_t machine_pin_type;
 // extern const mp_obj_type_t machine_timer_type;
 extern const mp_obj_type_t machine_i2c_type;
-// extern const mp_obj_type_t machine_wdt_type;
+extern const mp_obj_type_t machine_wdt_type;
 // extern const mp_obj_type_t machine_uart_type;
 
 #endif 
