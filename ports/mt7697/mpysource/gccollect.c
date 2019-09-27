27c0eea1ae90dd97bc7577dc2619155c5b18f37d
Tom Lin <tomlin.ntust@gmail.com>
2019-09-27 21:29:15 +0800

    mt7697/gcoollect.c: Modify about developing messages.

--- ports/mt7697/mpysource/gccollect.c
+++ ports/mt7697/mpysource/gccollect.c
@@ -37,41 +37,19 @@ uintptr_t gc_helper_get_regs_and_sp(uintptr_t *regs);
 extern uint32_t __StackTop;
 
 void gc_collect(void) {
-    // get current time, in case we want to time the GC
-    #if 0
-    uint32_t start = mp_hal_ticks_us();
-    #endif
-
     // start the GC
     gc_collect_start();
 
     // get the registers and the sp
-    uintptr_t regs[10];
-    uintptr_t sp = gc_helper_get_regs_and_sp(regs);
+    mp_uint_t regs[10];
+    mp_uint_t sp = gc_helper_get_regs_and_sp(regs);
 
     // trace the stack, including the registers (since they live on the stack in this function)
-    #if MICROPY_PY_THREAD
-    gc_collect_root((void**)sp, ((uint32_t)MP_STATE_THREAD(stack_top) - sp) / sizeof(uint32_t));
-    #else
-    gc_collect_root((void**)sp, ((uint32_t)&__StackTop - sp) / sizeof(uint32_t));
-    #endif
-
-    // trace root pointers from any threads
-    #if MICROPY_PY_THREAD
+#if MICROPY_PY_THREAD
+    gc_collect_root( (void **)sp, ((uint32_t)MP_STATE_THREAD(stack_top) - sp) / sizeof(uint32_t));
     mp_thread_gc_others();
-    #endif
-
-    // end the GC
+#else
+    gc_collect_root( (void **)sp, ((uint32_t)& __StackTop - sp) / sizeof(uint32_t));
+#endif
     gc_collect_end();
-
-    #if 0
-    // print GC info
-    uint32_t ticks = mp_hal_ticks_us() - start;
-    gc_info_t info;
-    gc_info(&info);
-    printf("GC@%lu %lums\n", start, ticks);
-    printf(" " UINT_FMT " total\n", info.total);
-    printf(" " UINT_FMT " : " UINT_FMT "\n", info.used, info.free);
-    printf(" 1=" UINT_FMT " 2=" UINT_FMT " m=" UINT_FMT "\n", info.num_1block, info.num_2block, info.max_block);
-    #endif
 }
